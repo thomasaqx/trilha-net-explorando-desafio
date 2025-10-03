a58b5410b2841f2cea61d4383c65c1dd10e582b6
thomas <thomasjml10@gmail.com>
2025-10-03 15:08:33 -0300

    realizando alterações propostas

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,43 +15,46 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            if (Suite.Capacidade >= hospedes.Count)
             {
                 Hospedes = hospedes;
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                throw new ArgumentException("A capacidade da suíte é menor do que o número de hóspedes.");
             }
         }
 
         public void CadastrarSuite(Suite suite)
         {
+            if (suite == null)
+            {
+                throw new ArgumentNullException(nameof(suite), "A suíte não pode ser nula.");
+            }
             Suite = suite;
         }
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            if (Hospedes == null)
+            {
+                return 0;
+            }
+            return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
-
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            if (Suite == null)
             {
-                valor = 0;
+                throw new InvalidOperationException("Não é possível calcular o valor da diária sem uma suíte cadastrada.");
+            }
+
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            if (DiasReservados >= 10)
+            {
+                valor *= 0.90M; 
             }
 
             return valor;
